1efd0dbac08a407bb0e30f1abcf441c4c1b7e273
Zoe Barrington <zoe.barrington@accenture.com>
2024-02-27 18:31:19 +0000

    not working yet - but need to try calling from home laptop where google account is zlfbarrington@gmail.com

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
     MidpointCalculator midpointCalculator = new MidpointCalculator();
     ClosestTubeCalculator closestTubeCalculator = new ClosestTubeCalculator();
     double[] midpointCoordinates = midpointCalculator.getGoogleDirections("e35pd", "sw197qu");
     String mapsUrl = closestTubeCalculator.buildNearbyTubeUrl(midpointCoordinates[0], midpointCoordinates[1], "1000");
         System.out.println(mapsUrl);
+        closestTubeCalculator.getClosestTubeTesting();
     }
 }
